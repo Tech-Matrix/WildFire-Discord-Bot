84946df9ee9cae487e6465ed21d911330d082b2d
RohanJnr <rohanjnr44@gmail.com>
2021-09-05 01:02:07 +0530

    Add logging config.

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,25 @@
 import logging
+from logging import handlers
+from pathlib import Path
 
-# TODO: configure logging.
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logging_file = Path("logs", f"bot.log")
+logging_file.parent.mkdir(exist_ok=True)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+file_handler = handlers.RotatingFileHandler(logging_file, maxBytes=524, backupCount=3, encoding="utf8")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+logging.getLogger("lightbulb").setLevel(logging.INFO)
+logging.getLogger("hikari").setLevel(logging.INFO)
