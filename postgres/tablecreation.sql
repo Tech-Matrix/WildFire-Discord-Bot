1040faf272db8b692f8328a38df8ea92330fc79f
nameetak-22 <nameeta.kuruwatti@gmail.com>
2021-09-09 23:26:18 +0530

    new table added

--- postgres/tablecreation.sql
+++ postgres/tablecreation.sql
@@ -1,5 +1,11 @@
 CREATE TABLE IF NOT EXISTS offences {
-    id INTEGER NOT NULL PRIMARY KEY,
+    server_id VARCHAR(30) NOT NULL,
+    id VARCHAR(30) NOT NULL PRIMARY KEY,
     num_of_offences INTEGER NOT NULL
+}
+
+CREATE TABLE IF NOT EXISTS filter {
+    server_id bigint NOT NULL,
+    filter_pattern varchar(256) NOT NULL
 }
 
