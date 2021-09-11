2007efbf0a70fe99e0b0550d0a3c2ee959b2530f
nameetak-22 <nameeta.kuruwatti@gmail.com>
2021-09-11 15:23:14 +0530

    added table prefixes

--- postgres/tablecreation.sql
+++ postgres/tablecreation.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS offences {
+CREATE TABLE IF NOT EXISTS offences (
     server_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
     num_of_offences INTEGER NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS filter {
+CREATE TABLE IF NOT EXISTS filter (
     server_id BIGINT NOT NULL,
     filter_pattern VARCHAR(256) NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS prefixes {
+CREATE TABLE IF NOT EXISTS prefixes (
     server_id BIGINT NOT NULL,
     prefix VARCHAR(16) NOT NULL
-};
+);
 
